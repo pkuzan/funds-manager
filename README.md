ab3774757eb229b9cd066c7bb32129e6fb129c4d
pkuzan <paul_kuzan@hotmail.com>
2018-08-28 09:57:55 +0100

    AWS CodeDeploy stuff complete

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Funds Manager
+
+To run the application locally, set a Spring Profile = local.
--- funds-manager-angular-app/src/main/code-deploy/scripts/start_server.sh
+++ funds-manager-angular-app/src/main/code-deploy/scripts/start_server.sh
@@ -5,18 +5,18 @@
 # autoscale group by the cloud formation script. This is needed as the
 # environment variables are not automatically picked up when running as root
 # even through they are in the /etc/profile area
-source /etc/profile.d/gt-cloud.sh
-mkdir -p /var/log/gt-cloud-pricer-default
-if [ -f /var/log/gt-cloud-pricer-default/gt-cloud-pricer.log ]; then mv /var/log/gt-cloud-pricer-default/gt-cloud-pricer.log "/var/log/gt-cloud-pricer-default/gt-cloud-pricer.log.$(date +"%Y%m%d_%H%M%S")"; fi
-touch /var/log/gt-cloud-pricer-default/gt-cloud-pricer.log
+# source /etc/profile.d/gt-cloud.sh
+mkdir -p /var/log/funds-manager
+if [ -f /var/log/funds-manager/funds-manager.log ]; then mv /var/log/funds-manager/funds-manager.log "/var/log/funds-manager/funds-manager.log.$(date +"%Y%m%d_%H%M%S")"; fi
+touch /var/log/funds-manager/funds-manager.log
 
 # The application must not take over stdin, stdout or stderr streams otherwise
 # code deploy will fail to work correctly.
 # see http://docs.aws.amazon.com/codedeploy/latest/userguide/troubleshooting-deployments.html#troubleshooting-long-running-processes for details
-java -jar /opt/gt-cloud-pricer-default/pricer-default.jar 2> /dev/null > /dev/null < /dev/null &
+java -jar /opt/funds-manager/funds-manager-angular-app.jar 2> /dev/null > /dev/null < /dev/null &
 
 # Wait to see if a successful startup.
-tail -f /var/log/gt-cloud-pricer/gt-cloud-pricer.log | while read LOGLINE
+tail -f /var/log/funds-manager/funds-manager.log | while read LOGLINE
 do
     [[ "${LOGLINE}" == *"Tomcat started on port(s)"* ]] && pkill -P $$ tail
 done
