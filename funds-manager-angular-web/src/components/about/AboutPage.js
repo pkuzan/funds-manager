33e0ad25102381ddb33977fa1bd672ba3d33d197
pkuzan <paul_kuzan@hotmail.com>
2018-08-28 17:27:34 +0100

    More JS

--- funds-manager-angular-web/src/components/about/AboutPage.js
+++ funds-manager-angular-web/src/components/about/AboutPage.js
@@ -5,13 +5,13 @@ class AboutPage extends React.Component {
     render(){
         return {
             <div>
-
+                {this.renderAbout()}
             </div>
         }
     }
 
     renderAbout(){
-
+        <p>About</p>
     }
 
 }
---
+++ funds-manager-angular-web/src/main.js
@@ -0,0 +1,27 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import 'whatwg-fetch';
+import {BrowserRouter} from 'react-router=dom';
+import {AppplicationContainer} from 'react-hot-loader';
+import App from './components/App';
+import 'styles/app.css'
+
+const render = (Component) => {
+    ReactDOM.render(
+        <AppContainer>
+            <BrowserRouter>
+                <Component />
+            </BrowserRouter>
+        </AppContainer>
+        document.getElementById('app')
+    );
+};
+
+render(App);
+
+if(module.hot){
+    module.hot.accept('./components/App', () => {
+        const NextApp = require('./components/App').default;
+        render(NextApp);
+    });
+}
